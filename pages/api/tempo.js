325b574d7bc5c447347cd7b6882b62095ec80dae
José Carlos de Gasperi <78042585+jcgfip@users.noreply.github.com>
2021-02-03 12:29:54 -0300

    ops

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,7 +2,7 @@ function tempo(request, response){
     const dynamicDate = new Date();
 
     /* responsável por dar menos requisições na vercel */
-    response.setHeader('Cache-Control', 's-maxage=10, state-while-revalidate');
+    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
     response.json({
         date: dynamicDate.toGMTString()
