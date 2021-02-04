52792bb6c94791934b9c0916cf6009506e994248
José Carlos de Gasperi <78042585+jcgfip@users.noreply.github.com>
2021-02-04 10:13:54 -0300

    tentando carregar logo

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import {useState} from 'react';
 import Link from 'next/link';
 
 function Home(){
-    return <div>
-        <h1>FIP</h1>
-        
+    return <body>
+    <div>
+        <Logotipo />
+        <p></p>
         <Contador />
-        
             <p></p>
             <Link href="/sobre">
                 <a>Sobre</a>
@@ -18,28 +18,35 @@ function Home(){
                 </Link>
             </div>
 
+        </div>
+
         <footer>
             <p> Em desenvolvimento ~ by JC</p>
             <a href="http://www.fip.com.br" target="blank">fip.com.br</a>
         </footer>
 
-        </div>
+    </body>
 }
 
 function Contador(){
     const [contador, setContador] = useState(1);
-    
     function adicionarContador(){
         setContador(contador + 1);
     }
-
     return(
         <div>
             <div>{contador}</div>
             <button onClick={adicionarContador}>Adicionar</button>
         </div>
     )
+}
 
+function Logotipo(){
+return(
+    <div>
+        <img src='./_images/fip.png' alt=' fip' width='100' height='100'></img>
+    </div>
+    )
 }
 
 export default Home
